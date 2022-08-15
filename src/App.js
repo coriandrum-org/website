869c4548012e0ada0bd31e40cfdd25935ea26e17
Antoni <antoni.protoni@gmail.com>
2022-08-15 15:40:58 -0400

    comming soon..

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
+import logo2 from './logo2.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo2} className="App-logo2" alt="logo" />
+        <h1>
+          Comming soon...
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coriandrum org
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
